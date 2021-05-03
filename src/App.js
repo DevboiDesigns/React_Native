fdda850137ff403d37d45a69a578b5bb6fc5fc85
Christopher <visrule@protonmail.com>
2021-05-03 16:38:42 -0500

    functionality design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import CardList from './CardList'
+import SearchBox from './SearchBox'
+import { robots } from './robots.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state ={
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    // your own made up Functions need  " = () => "
+    onSearchChange = (event) => {
+
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })              // .includes()  .filter()   .toLowerCase()
+
+        return (
+            <div className='tc'>
+
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots}/> 
+    
+            </div>
+           
+        )
+    }
 }
 
-export default App;
+export default App
